255db4903a8d55f40f38e3116fb926dcf612ad98
TomNemeth <tomnemeth210@gmail.com>
2016-12-06 08:17:33 -0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,7 +1,7 @@
 buildMap(nomer){
   var ele = document.getElementById(nomer);
   
-  var list1 = document.createElement("li");
+/*  var list1 = document.createElement("li");
   var list2 = document.createElement("li");
   var list3 = document.createElement("li");
   var sub1list1 = document.createElement("li");
@@ -22,9 +22,13 @@ buildMap(nomer){
   sub1list5.textContent = "Chalkboard";
   sub1list6.textContent = "Tic Tac Toe";
   sub1list7.textContent = "Multiplication Table";
+*/  
   
+  var para = document.createElement("li");                      // Create a <li> element
+  var t = document.createTextNode("This is a paragraph.");      // Create a text node
+  para.appendChild(t);                                          // Append the text to <li>
+  document.getElementById("nomer").appendChild(para);           // Append <li> 
   
-  
-  ele.appendChild((document.createElement("ul")));
+  //ele.appendChild((document.createElement("ul")));
   
 }
