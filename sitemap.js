c665e18bd31dec71e41445ee5308daa4fae77852
TomNemeth <tomnemeth210@gmail.com>
2016-12-06 09:02:34 -0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -4,68 +4,62 @@ function buildMap(x){
     var listItem = document.createElement('LI');
     y.appendChild(bullet);
     
-    var linkIndex = document.createElement('A');
+    var linkHome = document.createElement('A');
     var listItem = document.createElement('LI');
-    linkIndex.setAttribute("href", "index.html");
-    linkIndex.textContent = "Index";
+    linkHome.setAttribute("href", "index.html");
+    linkHome.textContent = "Home";
     listItem.appendChild(linkIndex);
     bullet.appendChild(listItem);
     
     var linkBio = document.createElement('A');
     var listItem = document.createElement('LI');
     linkBio.setAttribute("href", "aboutme.html");
-    linkBio.textContent = "Bio";
+    linkBio.textContent = "About me";
     listItem.appendChild(linkBio);
     bullet.appendChild(listItem);
     
     var listItem = document.createElement('LI');
-    listItem.textContent = "Class Projects";
+    listItem.textContent = "Class Projects:";
     bullet.appendChild(listItem);
     
-    var nestedList = document.createElement('UL');
+    var List2 = document.createElement('UL');
     listItem.appendChild(nestedList);
     
     var linkBreakout = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
+    var subListItem = document.createElement('LI');
     linkBreakout.setAttribute("href", "breakout.html");
-    linkBreakout.textContent = "Breakout";
-    nestedListItem.appendChild(linkBreakout);
-    nestedList.appendChild(nestedListItem);
+    linkBreakout.textContent = "Something that resembles Breakout";
+    subListItem.appendChild(linkBreakout);
+    List2.appendChild(nestedListItem);
     
-    var linkJSEvents = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
-    linkJSEvents.setAttribute("href", ".html");
-    linkJSEvents.textContent = "JavaScript Events";
-    nestedListItem.appendChild(linkJSEvents);
-    nestedList.appendChild(nestedListItem);
+    var linkJavaEvent = document.createElement('A');
+    var subListItem = document.createElement('LI');
+    linkJavaEvent.setAttribute("href", "background-game.html");
+    linkJavaEvent.textContent = "JavaScript Events";
+    subListItem.appendChild(linkJavaEvent);
+    List2.appendChild(subListItem);
     
-    var linkEureka = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
-    linkEureka.setAttribute("href", "popup.html");
-    linkEureka.textContent = "Pop-Up!";
-    nestedListItem.appendChild(linkEureka);
-    nestedList.appendChild(nestedListItem);
     
     var linkChalkboard = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
+    var subListItem = document.createElement('LI');
     linkChalkboard.setAttribute("href", "chalkboard.html");
     linkChalkboard.textContent = "Chalkboard";
-    nestedListItem.appendChild(linkChalkboard);
-    nestedList.appendChild(nestedListItem);
+    subListItem.appendChild(linkChalkboard);
+    List2.appendChild(subListItem);
     
     var linkTicTacToe = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
+    var subListItem = document.createElement('LI');
     linkTicTacToe.setAttribute("href", "tictactoe.html");
     linkTicTacToe.textContent = "Tic-Tac-Toe";
-    nestedListItem.appendChild(linkTicTacToe);
-    nestedList.appendChild(nestedListItem);
+    subListItem.appendChild(linkTicTacToe);
+    List2.appendChild(subListItem);
     
     var linkMultiplication = document.createElement('A');
-    var nestedListItem = document.createElement('LI');
+    var subListItem = document.createElement('LI');
     linkMultiplication.setAttribute("href", "multTable.html");
     linkMultiplication.textContent = "Multiplication";
-    nestedListItem.appendChild(linkMultiplication);
-    nestedList.appendChild(nestedListItem);
+    subListItem.appendChild(linkMultiplication);
+    List2.appendChild(subListItem);
     
 
 }
